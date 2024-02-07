2b2fc5e9f6d233e06e1ad0f51677d8b6c8a1bcab
yuutoharada <yuuto.harada@arsaga.jp>
2024-02-07 15:18:21 +0900

    編集画面UIの修正

--- src/pages/users/userEditPage/[id].tsx
+++ src/pages/users/userEditPage/[id].tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { Spin } from "antd";
 
 export default function UserEditPage() {
   const {
@@ -83,31 +84,31 @@ export default function UserEditPage() {
   return userDataIsLoading || tenantsDataIsLoading ? (
     <></>
   ) : (
-    <div className="max-w-[50rem] mx-auto p-[5rem]">
+    <div className="max-w-[100rem] mx-auto p-[4.5rem]">
       <form onSubmit={handleSubmit(onSubmit)} className="w-full bg-white">
-        <div className="grid grid-cols-3 gap-[1.5rem]">
-          <label className="grid justify-end items-center text-[1rem]">
+        <div className="grid grid-cols-3 gap-[1.2rem] ml-[15rem] mr-[8rem]">
+          <label className="grid justify-end items-center text-[1rem] col-span-1">
             ユーザー名
           </label>
-          <div className="grid justify-start items-center col-span-2">
+          <div className="grid justify-start items-center col-span-1">
             <input
               {...register("app_metadata.user_name", {
                 required: "※入力は必須です",
               })}
-              className="h-[2.5rem] rounded-sm border-[0.1rem] border-main text-[1.2rem] min-w-[20rem]"
+              className="h-[2rem] rounded-sm border-[0.1rem] border-main text-[1rem] min-w-[15rem] pl-[0.3rem]"
               type="text"
               defaultValue={user ? user.user_name : ""}
             />
-            <div className="text-red-500">
-              {errors.app_metadata?.user_name &&
-                errors.app_metadata?.user_name.message}
-            </div>
+          </div>
+          <div className="text-red-500 grid justify-start items-center col-span-1">
+            {errors.app_metadata?.user_name &&
+              errors.app_metadata?.user_name.message}
           </div>
 
           <label className="grid justify-end items-center pt-[1.5rem] text-[1rem]">
             メールアドレス
           </label>
-          <div className="grid justify-start items-center col-span-2 pt-[1.5rem]">
+          <div className="grid justify-start items-center col-span-1 pt-[1.5rem]">
             <input
               {...register("email", {
                 required: "※入力は必須です",
@@ -115,26 +116,27 @@ export default function UserEditPage() {
                   setIsUsedEmail("");
                 },
               })}
-              className="h-[2.5rem] rounded-sm border-[0.1rem] border-main text-[1.2rem] min-w-[20rem]"
+              className="h-[2rem] rounded-sm border-[0.1rem] border-main text-[1rem] min-w-[15rem] pl-[0.3rem]"
               type="email"
               autoComplete="email"
               defaultValue={user ? user.email : ""}
             />
-            <div className="text-red-500">
-              {errors.email && errors.email.message}
-            </div>
-            <div className="text-red-500">{isUsedEmail}</div>
+          </div>
+          <div className="text-red-500 grid justify-start items-center col-span-1 pt-[1.5rem]">
+            {errors.email && isUsedEmail === ""
+              ? errors.email.message
+              : isUsedEmail && isUsedEmail}
           </div>
 
           <label className="grid justify-end items-center pt-[1.5rem] text-[1rem]">
             所属
           </label>
-          <div className="grid justify-start items-center col-span-2 pt-[1.5rem]">
+          <div className="grid justify-start items-center col-span-1 pt-[1.5rem]">
             <select
               {...register("app_metadata.tenant_id", {
                 required: "※入力は必須です",
               })}
-              className="h-[2.3rem] rounded-sm border-[0.1rem] border-main text-[1.2rem] min-w-[20.4rem]"
+              className="h-[2.3rem] rounded-sm border-[0.1rem] border-main text-[1rem] min-w-[15.6rem] pl-[0.1rem]"
               defaultValue={user.tenant_id}
             >
               {tenants &&
@@ -144,79 +146,81 @@ export default function UserEditPage() {
                   </option>
                 ))}
             </select>
-            <div className="text-red-500">
-              {errors.app_metadata?.tenant_id &&
-                errors.app_metadata?.tenant_id.message}
-            </div>
+          </div>
+          <div className="text-red-500 grid justify-start items-center col-span-1 pt-[1.5rem]">
+            {errors.app_metadata?.tenant_id &&
+              errors.app_metadata?.tenant_id.message}
           </div>
 
           <label className="grid justify-end items-center pt-[1.5rem] text-[1rem]">
             新しいパスワード
           </label>
-          <div className="grid justify-start items-center col-span-2 pt-[1.5rem]">
+          <div className="grid justify-start items-center col-span-1 pt-[1.5rem]">
             <input
               {...register("password", {
                 required: "※入力は必須です",
                 validate: (value: string) => {
-                  return value.length >= 8 || "8文字以上で作成してください";
+                  return value.length >= 8 || "※8文字以上で作成してください";
                 },
               })}
-              className="h-[2.5rem] rounded-sm border-[0.1rem] border-main text-[1.2rem] min-w-[20rem]"
+              className="h-[2rem] rounded-sm border-[0.1rem] border-main text-[1rem] min-w-[15rem] pl-[0.3rem]"
               type="password"
               autoComplete="new-password"
             />
-            <div className="text-red-500">
-              {errors.password && errors.password.message}
-            </div>
+          </div>
+          <div className="text-red-500 grid justify-start items-center col-span-1 pt-[1.5rem]">
+            {errors.password && errors.password.message}
           </div>
 
           <label className="grid justify-end items-center pt-[1.5rem] text-[1rem]">
             新しいパスワード（確認）
           </label>
-          <div className="grid justify-start items-center col-span-2 pt-[1.5rem]">
+          <div className="grid justify-start items-center col-span-1 pt-[1.5rem]">
             <input
               {...register("passwordConf", {
                 required: "※入力は必須です",
                 validate: (value) => {
                   if (value !== getValues("password")) {
-                    return "パスワードが一致しません";
+                    return "※パスワードが一致しません";
                   }
                 },
               })}
-              className="h-[2.5rem] rounded-sm border-[0.1rem] border-main text-[1.2rem] min-w-[20rem]"
+              className="h-[2rem] rounded-sm border-[0.1rem] border-main text-[1rem] min-w-[15rem] pl-[0.3rem]"
               type="password"
               autoComplete="new-password"
             />
-            <div className="text-red-500">
-              {errors.passwordConf && errors.passwordConf.message}
-            </div>
+          </div>
+          <div className="text-red-500 grid justify-start items-center col-span-1 pt-[1.5rem]">
+            {errors.passwordConf && errors.passwordConf.message}
           </div>
         </div>
-        <div className="grid grid-cols-2 pt-[5rem]">
-          <div className="grid justify-center">
+        <div className="flex justify-center items-center mt-[5rem] mr-[0.8rem]">
+          <div className="">
             <button
               type="button"
               onClick={() => {
                 router.push("/users");
               }}
-              className="mx-[1rem] text-[#153F8D] underline underline-offset-[0.2rem] decoration-[#153F8D] border-none hover:text-[#008E92] hover:decoration-[#008E92] focus:outline-none"
+              className="mx-[0.8rem] text-[#153F8D] underline underline-offset-[0.2rem] decoration-[#153F8D] border-none hover:text-[#008E92] hover:decoration-[#008E92] focus:outline-none"
             >
               一覧に戻る
             </button>
           </div>
-          <div>
+          <div className="">
             <button
               type="button"
               onClick={() => {
                 onClickDeleteButton();
               }}
-              className="mx-[1rem] bg-[#FFB800] text-white rounded-none focus:outline-none"
+              className="mx-[0.8rem] bg-[#FFB800] text-white rounded-none focus:outline-none"
             >
               削除する
             </button>
+          </div>
+          <div className="">
             <button
               type="submit"
-              className="mx-[1rem] bg-[#153F8D] text-white rounded-none focus:outline-none"
+              className="mx-[0.8rem] bg-[#153F8D] text-white rounded-none focus:outline-none"
             >
               保存する
             </button>
