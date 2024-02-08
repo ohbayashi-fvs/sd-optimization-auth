4206a9cda692aa2c96df9475c3a230c3b78cbf64
yuutoharada <yuuto.harada@arsaga.jp>
2024-02-08 13:21:16 +0900

    package.jsonインストールし直し

--- supabase/node_modules/dbdocs/src/hooks/init/checkUpdate.js
+++
@@ -1,6 +0,0 @@
-const updateNotifier = require('update-notifier');
-const pkg = require('../../../package.json');
-
-module.exports = async function () {
-  updateNotifier({ pkg }).notify({ isGlobal: true });
-};
