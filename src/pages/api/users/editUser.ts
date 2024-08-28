54aa6798d782f9fa387d80e20dd62f7ad346f5f5
Jun Ohbayashi <jun.ohbayashi@arsaga.jp>
2024-08-28 18:08:38 +0900

    editがうまくいかなかったので、修正

--- src/pages/api/users/editUser.ts
+++ src/pages/api/users/editUser.ts
@@ -1,18 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { createClient } from "../auth/createClient";
-import checkLogin from "../auth/session";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { createClient } from '../auth/createClient'
+import checkLogin from '../auth/session'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // session確認
-  const { isLogin } = await checkLogin(req, res);
+  const { isLogin } = await checkLogin(req, res)
 
   if (isLogin) {
     const supabaseServerClient = createClient({
       req,
       res,
-    });
-    const user = await JSON.parse(req.body);
+    })
+    const user = await JSON.parse(req.body)
 
     const data = await supabaseServerClient.auth.admin.updateUserById(user.id, {
       email: user.email,
@@ -22,11 +22,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         tenant_id: user.tenant_id,
         user_name: user.user_name,
       },
-    });
-
-    data.error?.status === 500 && res.status(500).json({});
-    res.status(200).json({});
+    })
+    console.log(data.error)
+    if (data.error) return res.status(401).json({ message: data.error.message })
+    res.status(200).json({})
   } else {
-    res.status(401).json({});
+    res.status(401).json({})
   }
-};
+}
